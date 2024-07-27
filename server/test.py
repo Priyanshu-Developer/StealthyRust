d408c2bd9f2deddf62b5e5bad8c021d11bd7562f
Priyanshu-Developer <priyanshu1kumar2002@gmail.com>
2024-07-27 22:16:05 +0530

    deleted

--- server/test.py
+++
@@ -1,4 +0,0 @@
-d="""lnjhkjljljohoohohohohgiu7yugfutguguf6fyfffyqwlmclejvgojivjenvkejvibivjenoejvienvke knekvjeoj kdn ko eokpeifojicwp[dopioeico[wpcw
-x   ]
-[x] =oqxskcnkncienjnksn ksn jb ksjoklk klcnsknkijockwpkp]po]pp]pppopop920350924!@#$%^^&&]]kasnvnkvndknvkdnvn"""
-print(len(d))
